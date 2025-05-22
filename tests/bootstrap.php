04b7881fd36cd1fe31a568de6e7e47a84c2d7715
Paulo <p.peresjr@gmail.com>
2025-05-22 16:20:06 -0300

    Add PHPUnit tests for Slack class with stubs

---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+require __DIR__ . '/stubs/Requests.php';
+require __DIR__ . '/stubs/wp_functions.php';
+
+require dirname(__DIR__) . '/includes/slack-interface/class-slack-access.php';
+require dirname(__DIR__) . '/includes/slack-interface/class-slack-team.php';
+require dirname(__DIR__) . '/includes/slack-interface/class-slack-api-exception.php';
+require dirname(__DIR__) . '/includes/slack-interface/class-slack.php';
