962bf799ef13d5223a923691993ca4ce02b149e0
Paulo <p.peresjr@gmail.com>
2025-05-22 16:17:29 -0300

    Add WordPress option stubs for tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+// Bootstrap file for PHPUnit tests.
+// Load WordPress option stubs so tests can run without WordPress.
+require_once __DIR__ . '/wp-option-stubs.php';
+
+// Additional bootstrap logic (autoload, etc.) can be placed here.
